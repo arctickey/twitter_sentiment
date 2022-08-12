de7a478b15d0b7731238f8dc1406b25b7ec60bdc
arctickey <filipchrzuszcz1@gmail.com>
2022-08-12 23:39:43 +0000

    Fixed git integration

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -2,5 +2,6 @@
 git config core.editor "code --wait"
 git config --global user.name "${GIT_USER}"
 git config --global user.email "${GIT_EMAIL}"
+git config --global --add url."git@github.com:".insteadOf "https://github.com/"
 poetry self update
 poetry install --no-interaction
